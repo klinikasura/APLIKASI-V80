9e6300087be59e49f8058fa339c39a6434ee1197
Klinik Asura <94031971+klinikasura@users.noreply.github.com>
2025-01-05 14:22:02 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();	
-
-    echo "<script>alert('Session Anda Telah Habis! Silahkan Login'); window.location = 'https://klinikasura.github.io/APLIKASI-V80/'</script>";
-?>
