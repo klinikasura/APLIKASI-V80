f053453bcfecf118fe9ba0b3696af6afb1dfb7ba
ASEP PUDIN NEW <94031971+klinikasura@users.noreply.github.com>
2026-01-30 15:52:19 +0700

    Add files via upload

---
+++ MIKROTIK/pasien2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Data Registrasi Pasien</title>
+  <meta http-equiv="refresh" content="0;url=10.10.20.1">
+  <style>
+    table {border-collapse:collapse;width:100%}
+    th,td {border:1px solid #ddd;padding:8px;text-align:left}
+    th {background:#f0f0f0}
+  </style>
+</head>
+<body>
+
+</body>
+</html>
+
